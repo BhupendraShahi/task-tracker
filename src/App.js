7955a914c68193a55b481c4b67d5d9db738b0e42
Bhupendra Shahi <shahi23.bhupendra11@gmail.com>
2021-02-14 19:07:00 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 
 function App() {
 
+  const[tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting aat School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Groceery Shopping',
+        day: 'Feb 7th at 4:30pm',
+        reminder: false,
+    },
+    
+])
+
+
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id?
+    {...task, reminder: !task.reminder}: task
+    )
+    )
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <AddTask onAdd={addTask}/>
+      { tasks.length > 0?
+        (<Tasks tasks={tasks} onDelete={deleteTask}
+        onToggle={toggleReminder} />):
+        ('No task to show')
+      }
     </div>
     
   );
